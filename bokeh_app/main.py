aaf1d1c25f3d1381557f609837db77563f786330
benrussell80 <bprussell80@gmail.com>
2020-04-16 15:02:07 -0700

    removed US tab to increase speed

--- bokeh_app/main.py
+++ bokeh_app/main.py
@@ -4,7 +4,7 @@ from bokeh.models import Tabs
 from bokeh.plotting import curdoc
 
 from world_map_tab import create_world_map_tab
-from us_map_tab import create_us_map_tab
+# from us_map_tab import create_us_map_tab
 from world_cases_time_series_tab import create_world_cases_time_series_tab
 from us_cases_time_series_tab import create_us_cases_time_series_tab
 from case_details_tab import create_case_details_tab
@@ -13,7 +13,7 @@ from analysis_tab import create_analysis_tab
 tabs = Tabs(
     tabs=[
         create_world_map_tab(),
-        create_us_map_tab(),
+        # create_us_map_tab(),
         create_world_cases_time_series_tab(),
         create_us_cases_time_series_tab(),
         create_case_details_tab(),
